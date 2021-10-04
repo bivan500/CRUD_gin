adeea721d67810157b4cf90e7d5325f4f274c15a
wannabewimp <vanericus@list.ru>
2021-10-05 00:14:27 +0300

    fix getListsQuery

--- pkg/repository/list_postgres.go
+++ pkg/repository/list_postgres.go
@@ -41,7 +41,8 @@ func (r *ListPostgres) Create(userId int, list crudApp.ReadList) (int, error) {
 
 func (r *ListPostgres) GetLists(userId int) ([]crudApp.ReadList, error) {
 	var result []crudApp.ReadList
-	getReadListQuery := fmt.Sprintf("SELECT * FROM %s INNER JOIN %s on lists.id = user_lists.list_id WHERE user_lists.users_id = $1", readListsTable, usersReadListTable)
+	getReadListQuery := fmt.Sprintf("SELECT lists.id,title,description FROM %s lists INNER JOIN %s user_lists on lists.id = user_lists.list_id WHERE user_lists.users_id = $1", readListsTable, usersReadListTable)
+	//getReadListQuery := "SELECT lists.id,title,description FROM lists INNER JOIN user_lists on lists.id = user_lists.list_id WHERE user_lists.users_id = 1"
 	err := r.db.Select(&result, getReadListQuery, userId)
 
 	return result, err
